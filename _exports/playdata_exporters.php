22b420950302d8f59156480535627b03bdd2f866
Ivey Padgett <iveysaurrr@gmail.com>
2015-09-08 16:28:06 -0400

    Export survey implemented.

--- _exports/playdata_exporters.php
+++ _exports/playdata_exporters.php
@@ -3,80 +3,144 @@
 return [
 	'Survey Formatting' => function($inst, $semesters_string)
 	{
-		$results = []
+		$csvs = [];
+		$headers = "User ID, Last Name, First Name, Semester, Game Time (seconds),";
 
 		foreach ($semesters_string as $semester)
 		{
 			list($year, $term) = explode('-', $semester);
 
 			// Get all scores for each semester
-			$logs = $play_logs["{$year} {$term}"] = \Materia\Session_Play::get_by_inst_id($inst->id, $term, $year);
+			$logs = \Materia\Session_Play::get_by_inst_id($inst->id, $term, $year);
 
+			$results = [];
 			foreach ($logs as $play)
 			{
-				// Only report actual user scores, no guests
-				if ( ! empty($play['username']))
-				{
-					if ( ! isset($results[$])) $results[$u] = [];
+				$play_id = $play['id'];
+				if ( ! isset($results[$play_id])) $results[$play_id] = [];
 
-					$play_events = \Materia\Session_Logger::get_logs($play['id']);
-					foreach ($play_events as $play_event)
-					{
-						$r               = [];
-						$r['last_name']  = $play['last'];
-						$r['first_name'] = $play['first'];
-						$r['playid']     = $play['id'];
-						$r['semester']   = $semester;
-						$r['type']       = $play_event->type;
-						$r['item_id']    = $play_event->item_id;
-						$r['text']       = $play_event->text;
-						$r['value']      = $play_event->value;
-						$r['game_time']  = $play_event->game_time;
-						$r['created_at'] = $play_event->created_at;
-						$results[$u][]   = $r;
-					}
+				$play_events = \Materia\Session_Logger::get_logs($play_id);
+				foreach ($play_events as $play_event)
+				{
+					$r                   = [];
+					$r['last_name']      = $play['last'] ? $play['last'] : "(Guest)";
+					$r['first_name']     = $play['first'];
+					$r['username']       = $play['username'];
+					$r['user_id']        = $play['user_id'];
+					$r['qset_id']        = $play['qset_id'];
+					$r['semester']       = $semester;
+					$r['type']           = $play_event->type;
+					$r['item_id']        = $play_event->item_id;
+					$r['text']           = $play_event->text;
+					$r['value']          = $play_event->value;
+					$r['game_time']      = $play_event->game_time;
+					$r['created_at']     = $play_event->created_at;
+					$results[$play_id][] = $r;
 				}
 			}
 
+			// If we didn't find any results, just return with nothing.
 			if ( ! count($results)) return false;
 
-			# Gotta grab the questions
-			$inst->get_qset($inst->id);
-			$questions = \Materia\Widget_Instance::find_questions($inst->qset->data);
-
-			# Legacy QSets follow the inane [items][items] scheme
-			if (isset($questions[0]) && isset($questions[0]['items']))
+			foreach ($results as $playid => $playlog)
 			{
-				$questions = $questions[0]['items'];
-			}
+				$qset_id = $playlog[0]['qset_id'];
 
-			$csv = "User ID, Last Name, First Name, Semester, Game Time,";
-			$question_columns = [];
-
-			# Build an array out of all possible questions. Note that not all columns will have an associated response
-			foreach ($quesions as $question)
-			{
-				# TODO: build string of column names based on each question (first X chars of question?), gets appended to $csv var
-				# TODO: build array of question IDs to match to log item IDs
-			}
-
-			foreach ($results as $userid => $userlog)
-			{
-				# TODO: create csv row using user data + user responses to each question in question array
-				# example: last name, first name, semester, game time, [first response...last response]
-
-				# So create first part of row here using user data
-
-				foreach ($userlog as $r)
+				// If we don't have the qset for this version, get it and do all of the setup.
+				if (empty($csvs[$qset_id]))
 				{
-					# Append each response to the row here
-					# If a response is logged for the same question TWICE, use the last response?
+					$cur_csv = $csvs[$qset_id] = [];
+					$cur_csv['questions'] = [];
+					$cur_csv['rows'] = [];
+					$cur_csv['timestamp'] = $playlog[0]['created_at'];
+
+					// Get the qset with this qset_id.
+					$qset = $inst->get_specific_qset($qset_id);
+
+					// Get the data from the qset and decode it then get questions from it.
+					$data = $qset[0]['data'];
+					$decoded_data = json_decode(base64_decode($data), true);
+					$questions = \Materia\Widget_Instance::find_questions($decoded_data);
+
+					// Legacy QSets follow the inane [items][items] scheme
+					if (isset($questions[0]) && isset($questions[0]['items']))
+					{
+						$questions = $questions[0]['items'];
+					}
+
+					// Question_text is the question headers in the form of a string.
+					$cur_csv['question_text'] = "";
+					foreach ($questions as $q)
+					{
+						$cur_csv['question_text'] .= $q->questions[0]['text'] . ', ';
+						$cur_csv['questions'][] = $q->id;
+					}
+
+					// Important to figure out where each response should go in the response array.
+					$cur_csv['num_questions'] = count($questions);
 				}
+				else
+				{
+					$cur_csv = $csvs[$qset_id];
+				}
+				// How many array spots there are. 5 for the original headers (name, semester, etc.),
+				// and however many questions there are.
+				$num_slots = 5 + $cur_csv['num_questions'];
+
+				// Check the game time to see if the user submitted a play.
+				$length = count($playlog);
+				if ($playlog[$length - 1]['game_time'] != -1)
+				{
+					$game_time = $playlog[$length - 1]['game_time'];
+				}
+				else
+				{
+					$game_time = 0;
+				}
+
+				// Array for the current row. Initialize with empty strings so when it is
+				// concatenated later it takes in account empty spots.
+				$logs = array_fill(0, $num_slots, '');
+				$logs[0] = $playlog[0]['user_id'];
+				$logs[1] = $playlog[0]['last_name'];
+				$logs[2] = $playlog[0]['first_name'];
+				$logs[3] = $playlog[0]['semester'];
+				$logs[4] = $game_time;
+
+				foreach ($playlog as $r)
+				{
+					// Append each response to the row.
+					// If a response is logged for the same question TWICE, use the last response?
+					if ($r['type'] == 'SCORE_QUESTION_ANSWERED')
+					{
+						// If the question id is in the question array for the current csv.
+						if (array_search($r['item_id'], $cur_csv['questions']) != FALSE)
+						{
+							$position = array_search($r['item_id'], $cur_csv['questions']);
+							$logs[5 + $position] = $r['text'];
+						}
+					}
+				}
+
+				$cur_csv['rows'][] = implode(", ", $logs);
+				$csvs[$qset_id] = $cur_csv;
 			}
 
-			# return the csv file
+			// Return the csv zip.
+			$tempname = tempnam('/tmp', 'materia_raw_log_csv');
+			$zip = new \ZipArchive();
+			$zip->open($tempname);
+			foreach ($csvs as $key => $csv)
+			{
+				$string = $headers . $csv['question_text'] . "\r\n" . implode("\r\n", $csv['rows']);
+				$zip->addFromString($inst->name . ' (created ' . date('m-d-y h_ia', $csv['timestamp'] ) . ').csv', $string);
+			}
+			$zip->close();
 
-			return [$csv, ".csv"];
+			$data = file_get_contents($tempname);
+			unlink($tempname);
+
+			return [$data, ".zip"];
 		}
 	},
 ];
