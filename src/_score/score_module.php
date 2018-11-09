95a347b87d575034a761c00d780c3dc8bede27f8
Ian Turgeon <iturgeon@gmail.com>
2018-11-09 15:40:58 -0500

    update package.json deps

--- src/_score/score_module.php
+++ src/_score/score_module.php
@@ -1,35 +1,9 @@
 <?php
-/**
- * Materia
- * It's a thing
- *
- * @package	    Materia
- * @version    1.0
- * @author     UCF New Media
- * @copyright  2011 New Media
- * @link       http://kogneato.com
- */
-
-
-/**
- * NEEDS DOCUMENTATION
- *
- * The widget managers for the Materia package.
- *
- * @package	    Main
- * @subpackage  scoring
- * @category    Modules
-  * @author      ADD NAME HERE
- */
 
 namespace Materia;
 
 class Score_Modules_Adventure extends Score_Module
 {
-	/**
-	 * NEEDS DOCUMENTATION
-	 * @param unknown NEEDS DOCUMENTATION
-	 */
 	public function check_answer($log)
 	{
 		# Adventure scores based on TYPE_FINAL_SCORE_FROM_CLIENT, ignore all other log types
